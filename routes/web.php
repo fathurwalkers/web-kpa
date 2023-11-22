3e54caeadc8a4fe0ba7520889840768d9f332565
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-21 21:43:31 -0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackController;
+use App\Http\Controllers\HomeController;
 
 Route::group(['prefix' => '/'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
