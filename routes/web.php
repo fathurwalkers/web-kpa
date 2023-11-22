74f0b08c3292c6772e6a8830e201785310dec52c
Fathur Walkers <fathurwalkers44@gmail.com>
2023-11-21 17:22:02 -0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group('home');
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::get('/karir', [HomeController::class, 'karir'])->name('karir');
+});
